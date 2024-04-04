e29cb1dadb7d19481546177dcc4da761eadd0b51
LuisaGiron <luisa.bermudez@mail.escuelaing.edu.co>
2024-04-04 17:54:49 -0500

    proyecto base del servicio habitacion

---
+++ src/main/java/arcn/roomfinder/habitacion/HabitacionApplication.java
@@ -0,0 +1,13 @@
+package arcn.roomfinder.habitacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arcn/roomfinder/habitacion/HabitacionApplicationTests.java
@@ -0,0 +1,13 @@
+package arcn.roomfinder.habitacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
